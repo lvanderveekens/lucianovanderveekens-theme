bd4dbee541648d3e1535b63ee62664fa4a3d6fbb
Luciano van der Veekens <luciano.van.der.veekens@ah.nl>
2021-12-21 16:16:05 +0100

    WIP: Experience section

--- front-page.php
+++ front-page.php
@@ -20,7 +20,7 @@ get_header();
 
 	<div id="about" class="container mx-auto px-4 md:mt-36">
 		<h1 class="text-4xl font-bold text-blue">About</h1>
-		<div class="grid grid-cols-1 sm:grid-cols-12 gap-4 items-center md:mt-20">
+		<div class="grid grid-cols-1 sm:grid-cols-12 gap-4 items-center justify-items-center md:mt-20">
 			<div class="sm:col-span-4">
 				<img src=<?php echo get_template_directory_uri() . "/images/luciano2.jpeg" ?> alt="Luciano">
 			</div>
@@ -30,20 +30,20 @@ get_header();
 				My main interests are sofware architecture and writing clean code.
 
 				Here's a list featuring the technologies I'm most comfortable with:
-				<div class="flex">
-					<ul class="colored-bullets">
+				<div class="flex flex-wrap">
+					<ul class="block colored-bullets">
 						<li>Java</li>
 						<li>Kotlin</li>
 						<li>Go</li>
 						<li>Spring Boot</li>
 					</ul>
-					<ul class="colored-bullets">
+					<ul class="block colored-bullets">
 						<li>Docker</li>
 						<li>Kubernetes</li>
 						<li>Postgres</li>
 						<li>MongoDB</li>
 					</ul>
-					<ul class="colored-bullets">
+					<ul class="block colored-bullets">
 						<li>Git</li>
 						<li>Maven</li>
 						<li>Jenkins</li>
@@ -58,8 +58,22 @@ get_header();
 		</div>
 	</div>
 
-	<div id="experience" class="">
-		<h1>Experience</h1>
+	<div id="experience" class="container mx-auto px-4 md:mt-36">
+		<h1 class="text-4xl font-bold text-blue">Experience</h1>
+		<div class="md:mt-20">
+			<div class="grid grid-cols-1 sm:grid-cols-12 gap-4 items-center ">
+				<div class="sm:col-span-3">
+				</div>
+				<div class="whitespace-pre-line sm:col-span-9">
+					I started out in a small team working on Albert Heijn for the Google Assistant which allowed customers to use their voice to, for example, search for recipes or add products to their shopping list.
+
+					Eventually that team grew and became responsible for setting up a digital AI assistant to automatically handle customer support queries or, in case it couldn’t figure out the customer’s intent, hand over the conversation to a human agent.
+
+					Technoglogies: Java - Kotlin - Spring Boot - Docker - Kubernetes - Postgres - MongoDB - Kafka
+				</div>
+			</div>
+
+		</div>
 	</div>
 
 	<div id="contact" class="">
