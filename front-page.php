5550830385e96ffb97ec9a75a63554c9ca1cf621
Luciano van der Veekens <luciano.van.der.veekens@ah.nl>
2021-12-21 21:03:02 +0100

    Finished Experience section

--- front-page.php
+++ front-page.php
@@ -60,12 +60,16 @@ get_header();
 
 	<div id="experience" class="container mx-auto px-4 md:mt-36">
 		<h1 class="text-4xl font-bold text-blue">Experience</h1>
-		<div class="md:mt-20">
-			<div class="grid grid-cols-1 sm:grid-cols-12 gap-4 items-center ">
-				<div class="sm:col-span-3">
+		<div>
+			<div class="grid grid-cols-1 sm:grid-cols-12 gap-4 md:my-20">
+				<div class="col-span-3">
+					<img class="experience-logo" src=<?php echo get_template_directory_uri() . "/images/ah-logo.png" ?> alt="Luciano">
+					<div class="font-bold">Software Engineer</div>
+					<div>Albert Heijn</div>
+					<div>Sep 2018 - Present</div>
+					<div>Zaandam, Netherlands</div>
 				</div>
-				<div class="whitespace-pre-line sm:col-span-9">
-					I started out in a small team working on Albert Heijn for the Google Assistant which allowed customers to use their voice to, for example, search for recipes or add products to their shopping list.
+				<div class="whitespace-pre-line sm:col-span-9 sm:col-start-4">I started out in a small team working on Albert Heijn for the Google Assistant which allowed customers to use their voice to, for example, search for recipes or add products to their shopping list.
 
 					Eventually that team grew and became responsible for setting up a digital AI assistant to automatically handle customer support queries or, in case it couldn’t figure out the customer’s intent, hand over the conversation to a human agent.
 
@@ -73,10 +77,45 @@ get_header();
 				</div>
 			</div>
 
+			<hr class="text-orange">
+
+			<div class="grid grid-cols-1 sm:grid-cols-12 gap-4 md:my-20">
+				<div class="col-span-3">
+					<img class="experience-logo" src=<?php echo get_template_directory_uri() . "/images/bol-com-logo.png" ?> alt="Luciano">
+					<div class="font-bold">Software Engineer</div>
+					<div>bol.com</div>
+					<div>Jan 2018 - Jul 2018</div>
+					<div>Utrecht, Netherlands</div>
+				</div>
+				<div class="whitespace-pre-line sm:col-span-9 sm:col-start-4">At bol.com I joined a team in the purchasing domain. Among other things our applications were used to manage warehouse replenishment according to some forecast.
+
+					Technologies: Kotlin - Spring Boot - Docker - Postgres
+				</div>
+			</div>
+
+			<hr class="text-orange">
+
+			<div class="grid grid-cols-1 sm:grid-cols-12 gap-4 md:my-20">
+				<div class="col-span-3">
+					<div class="experience-logo">
+						<img src=<?php echo get_template_directory_uri() . "/images/netinium-logo.png" ?> alt="Luciano">
+					</div>
+					<div class="font-bold">Software Engineer</div>
+					<div>Netinium</div>
+					<div>Mar 2015 - Dec 2017</div>
+					<div>Wormer, Netherlands</div>
+				</div>
+				<div class="whitespace-pre-line sm:col-span-9 sm:col-start-4">I began my career at Netinium, a small but innovative company that develops a single piece of software for energy providers to manage different types of smart electricity and gas meters.
+
+					Part of my day-to-day work included writing code to communicate with those smart meters using a standardized (Dutch) Smart Meter Requirement protocol.
+
+					Technologies: Java - Java EE - Oracle WebLogic Server - Oracle DB - Linux
+				</div>
+			</div>
 		</div>
 	</div>
 
-	<div id="contact" class="">
+	<div id="contact" class="container mx-auto px-4 md:mt-36">
 		<h1>Contact</h1>
 	</div>
 
