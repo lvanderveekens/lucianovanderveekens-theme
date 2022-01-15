a4dbc9538e009da3f55cb81caf24db7faff82547
Luciano van der Veekens <luciano.van.der.veekens@ah.nl>
2022-01-15 16:59:35 +0100

    Hide mobile nav after li click

--- js/navigation.js
+++ js/navigation.js
@@ -8,8 +8,12 @@
 
 	$( window ).on( 'resize', function() {
 		// TODO: read breakpoint from tailwind.config.js
-		if ( window.screen.width >= 768 ) {
+		if ( window.innerWidth >= 768 ) {
 			$( '.menu-container-mobile' ).removeAttr( 'style' );
 		}
+	} );
+
+	$( '.menu-container-mobile li' ).on( 'click', function() {
+		$( '.menu-container-mobile' ).removeAttr( 'style' );
 	} );
 }( jQuery ) );
